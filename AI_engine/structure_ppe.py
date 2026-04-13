788f911b0bef0bb9145605b3a5ef3b9f2e5c3166
광 <sj42k06@gmail.com>
2026-04-14 01:21:06 +0900

    Update structure_ppe.py

--- AI_engine/structure_ppe.py
+++ AI_engine/structure_ppe.py
@@ -1,13 +1,11 @@
 def structure_ppe_data(results_data):
     structured = []
-
     for frame_data in results_data:
         helmets = []
         vests = []
         machines = []
 
-        for det in frame_data["detections"]:
-
+        for det in frame_data.get("detections", []):
             obj_data = {
                 "id": None,
                 "type": det["type"],
@@ -16,14 +14,10 @@ def structure_ppe_data(results_data):
                 "cx": det["cx"],
                 "cy": det["cy"]
             }
-
-            # 카테고리 분류
             if det["type"] in ["Hardhat", "NO-Hardhat"]:
                 helmets.append(obj_data)
-
             elif det["type"] in ["Safety Vest", "NO-Safety Vest"]:
                 vests.append(obj_data)
-
             elif det["type"] in ["machinery", "vehicle"]:
                 machines.append(obj_data)
 
@@ -33,7 +27,8 @@ def structure_ppe_data(results_data):
             "height": frame_data["height"],
             "helmets": helmets,
             "vests": vests,
-            "machines": machines
+            "machines": machines,
+            "detections": frame_data.get("detections", [])
         })
 
     return structured
